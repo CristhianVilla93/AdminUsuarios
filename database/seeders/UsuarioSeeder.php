d422eba789d8beefdf1995e1cd448159a9238614
Cristhian Villa <crvm1993@gmail.com>
2023-08-18 19:01:33 -0500

    3comentario

--- database/seeders/UsuarioSeeder.php
+++ database/seeders/UsuarioSeeder.php
@@ -8,9 +8,7 @@ use Illuminate\Database\Seeder;
 
 class UsuarioSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
+    
     public function run(): void
     {
         Usuario::factory(100)->create();
